4f6c8ac3a10ad8e473eced9cbdfb075743f4fe17
ruffy <johan.ellfries@gmail.com>
2017-11-24 17:18:42 +0100

    add successful socket serving code.

--- server/index.js
+++ server/index.js
@@ -1,62 +1,64 @@
-   var gameport        = process.env.PORT || 3000,
+const gameport = process.env.PORT || 3000,
 
-        io              = require('socket.io'),
-        express         = require('express'),
-        UUID         = require('node-uuid'),
+io = require('socket.io'),
+express = require('express'),
+UUID = require('node-uuid');
 
-        logLevel = {
-            verbose: true,
-            info: true
-        };
+const logLevel = {
+        verbose: true,
+        info: true
+    };
 
-        var app = null,
-        server = null;
+let app = null,
+    server = null;
 
 setupExpressServer();
 setupSocketIO();
 startListening();
 
 function log(level, message) {
-    if(logLevel[level]) {
+    if (logLevel[level]) {
         console.log(message);
     }
 }
 
 function startListening() {
 
-   server.listen( gameport );
-        //Log something so we know that it succeeded.
-    console.log('Express: Listening on port ' + gameport );
+    server.listen(gameport);
+    console.log('Express: Listening on port ' + gameport);
 
 }
 
 function setupExpressServer() {
     app = express();
-    server = require('http').createServer(app);
+    server = require('http').Server(app);
 
-         //By default, we forward the / path to index.html automatically.
-    app.get( '/', function( req, res ){ 
-        res.sendFile( __dirname + '/index.html' );
+    //By default, we forward the / path to index.html automatically.
+    app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/index.html');
     });
 
-    app.get( '/*' , function( req, res, next ) {
-        var file = req.params[0]; 
-        log('verbose','Express: file requested : ' + file )
-        res.sendFile( __dirname + '/' + file );
+    app.get('/*', function(req, res, next) {
+        var file = req.params[0];
+        log('verbose', 'Express: file requested : ' + file)
+        res.sendFile(__dirname + '/' + file);
 
     });
 }
 
 function setupSocketIO() {
-    var sio = io.listen(server);
+    console.log('Setting upp socket.io..')
+    var sio = io(server);
 
-    sio.sockets.on('connection', function (client) {
+    sio.sockets.on('connection', function(client) {
         client.userid = UUID();
-        client.emit('onconnected', { id: client.userid } );
+        client.emit('onconnected', {
+            id: client.userid
+        });
         log('info', 'socket.io: player ' + client.userid + ' connected')
-        
-        client.on('disconnect', function () {
+
+        client.on('disconnect', function() {
             log('info', 'socket.io: client disconnected ' + client.userid)
-        }); 
-    }); 
+        });
+    });
 }
